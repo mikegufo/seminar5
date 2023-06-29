d18f0032ad539f4e4357347928d003353e70bf11
Vadim <nikegufo666@gmail.com>
2023-06-30 00:06:56 +0500

    Добавил две задачи

---
+++ Задача 34+/Program.cs
@@ -0,0 +1,35 @@
+﻿
+
+int[] FillArray()
+{
+    int[] array = new int[5];
+    Random rand = new Random();
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = rand.Next(100, 999);
+    } return array;
+}
+
+void PrintArray(int[] arrayForPrint)
+{
+    System.Console.WriteLine("[" + string.Join(", ", arrayForPrint) + "]");
+}
+
+void FindCount(int[] array, out int count)
+
+{
+    count = 0;
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 0)
+            count++;
+    }
+}
+
+int[] array = FillArray();
+PrintArray(array);
+FindCount(array, out int well);
+
+System.Console.WriteLine($"Количество четных чисел : {well}");
---
+++ Задача 36+/Program.cs
@@ -0,0 +1,34 @@
+﻿//Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечетных позициях.
+//[3, 7, 23, 12] => 19
+//[-4, -6, 89, 6] => 0
+
+int[] FillArray()
+{
+    int[] array = new int[5];
+    Random rand = new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = rand.Next(10, 99);
+    }
+    return array;
+}
+
+void PrintArray(int[] arrayForPrint)
+{
+    System.Console.WriteLine("[" + string.Join(", ", arrayForPrint) + "]");
+
+}
+
+void Sum(int[] array, out int sum)
+{
+    sum = 0;
+    for (int i = 0; i < array.Length; i += 2)
+        sum = sum + array[i];
+
+}
+
+int[] array = FillArray();
+PrintArray(array);
+Sum(array, out int well);
+
+Console.WriteLine($"Сумма элементов cтоящих на нечётных позициях = {well}");
